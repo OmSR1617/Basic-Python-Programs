67497c89942e8f28085f4096396912cf0437b0af
Sakshad Ramgude <70849480+OmSR1617@users.noreply.github.com>
2026-07-06 20:39:05 +0530

    check largest of 3 number program

---
+++ largest_number.py
@@ -0,0 +1,10 @@
+a = int(input("Enter Your 1st Number: "))
+b = int(input("Enter Your 2nd Number: "))
+c = int(input("Enter Your 3rd Number: "))
+
+if a>b and a>c:
+    print("A is Greater")
+elif b>a and b>c:
+    print("B is greater")
+else:
+    print("C is Greater")
