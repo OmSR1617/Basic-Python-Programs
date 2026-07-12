ba89885dff3d559f49bc0f356b8a3dcbaa2faa5a
Sakshad Ramgude <70849480+OmSR1617@users.noreply.github.com>
2026-07-12 15:59:40 +0530

    mini challenge 3

---
+++ mini_challenge3.py
@@ -0,0 +1,30 @@
+marks = []
+
+sub1 = int(input("Enter English Marks: "))
+sub2 = int(input("Enter Hindi Marks: "))
+sub3 = int(input("Enter Marathi Marks: "))
+sub4 = int(input("Enter Maths Marks: "))
+sub5 = int(input("Enter Science Marks: "))
+
+marks = [sub1, sub2, sub3, sub4, sub5]
+print("Marks Added!")
+
+marks.sort()
+print("Highest Marks is: ",marks[-1])
+
+print("Lowest Marks is: ",marks[0])
+
+average = (sum(marks) * 100) / 500
+print("Average is: ",average,"%")
+
+pass_count = 0
+fail_count = 0
+
+for sub in marks:
+    if sub >= 40:
+        pass_count += 1
+    else:
+        fail_count += 1 
+        
+print("Total Pass Count: ",pass_count)
+print("Total Fail Count: ",fail_count)
